bca96823408812abe875f8c5a34a2c2927f80b0c
gongzhipeng <gongzhioeng@corp.netease.com>
2018-11-20 19:18:36 +0800

    all-in-web版本

---
+++ all-in-web/src/main/java/com/allin/allinweb/AllInWebApplication.java
@@ -0,0 +1,12 @@
+package com.allin.allinweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllInWebApplication.class, args);
+    }
+}
