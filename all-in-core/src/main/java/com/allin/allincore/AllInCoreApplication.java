a891c131f97e4ecc563a252e061815df9f678601
gongzhipeng <gongzhioeng@corp.netease.com>
2018-11-20 19:18:08 +0800

    all-in-core

---
+++ all-in-core/src/main/java/com/allin/allincore/AllInCoreApplication.java
@@ -0,0 +1,12 @@
+package com.allin.allincore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllInCoreApplication.class, args);
+    }
+}
