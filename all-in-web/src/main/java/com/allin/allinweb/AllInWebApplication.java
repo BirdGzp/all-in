6f9f0ab81224d555cd2a99e35281b768ea0a3ecb
gongzhipeng <gongzhioeng@corp.netease.com>
2018-11-26 20:36:53 +0800

    push

--- all-in-web/src/main/java/com/allin/allinweb/AllInWebApplication.java
+++ all-in-web/src/main/java/com/allin/allinweb/AllInWebApplication.java
@@ -2,9 +2,13 @@ package com.allin.allinweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class AllInWebApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(AllInWebApplication.class, args);
