7a4c5b556b4d6e5ce0ac999d6b7500f4273617b4
gongzhipeng <gongzhioeng@corp.netease.com>
2018-11-26 20:35:54 +0800

    2018-11-26

--- study/src/main/java/JedisTest.java
+++
@@ -1,9 +0,0 @@
-import redis.clients.jedis.Jedis;
-
-public class JedisTest {
-    public static void main(String[] args) {
-        Jedis jedis = new Jedis("192.168.111.136");
-        System.out.println(jedis.ping());
-    }
-
-}
--- study/src/main/java/PoTest.java
+++
@@ -1,7 +0,0 @@
-import com.sun.javafx.geom.transform.BaseTransform;
-
-public class PoTest {
-    public static void main(String[] args) {
-
-    }
-}
