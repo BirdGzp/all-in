b91cad007b635f8ff2971f04d4fbeb14c97e4f76
gongzhipeng <gongzhioeng@corp.netease.com>
2018-11-28 11:49:13 +0800

    java

---
+++ all-in-config/src/main/java/com/allin/config/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.allin.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    /**
+     * /{application}/{profile}[/{label}]
+     * /{application}-{profile}.yml
+     * /{label}/{application}-{profile}.yml
+     * /{application}-{profile}.properties
+     * /{label}/{application}-{profile}.properties
+     */
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
